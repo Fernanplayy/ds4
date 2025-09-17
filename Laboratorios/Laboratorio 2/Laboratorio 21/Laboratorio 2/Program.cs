feb70ecd36400faefc19b49b02109ceec8474eb8
Fernan <fernangamergamer23@gmail.com>
2025-09-16 19:36:08 -0500

    Se agrego el ejemplo 1 del laboratorio 2

---
+++ Laboratorios/Laboratorio 2/Laboratorio 21/Laboratorio 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Laboratorio_21;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Client client = new Client();
+
+        client.FirstName = "Su_Nombre";
+        client.LastName = "Su_Apellido";
+        client.Age = 15;
+        client.Id = 1;
+
+        Console.WriteLine(client.GetFullName());
+    }
+}
+
+public class Client
+{
+    //Definir atributos de la clase
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    
+    //Definir el método GetFullName que retorna el nombre completo del cliente
+    public string GetFullName()
+    {
+        return FirstName +" "+ LastName;
+    }
+}
