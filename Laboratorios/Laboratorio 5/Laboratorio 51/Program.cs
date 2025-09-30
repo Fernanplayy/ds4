710f78341d6db235efab36eef449539b91424fb5
Fernanplayy <fernangamergamer23@gmail.com>
2025-09-30 00:29:08 -0500

    Laboratorio 5 - En desarrollo

---
+++ Laboratorios/Laboratorio 5/Laboratorio 51/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    private int[] sueldos;
+
+    private void Cargar() 
+    {
+        sueldos = new int[6];
+        for (int f = 1; f < 5; f++)
+        {
+            Console.Write("Ingrese el sueldo del operario "+f+": ");
+            String linea;
+            linea = Console.ReadLine();
+            sueldos[f] = int.Parse(linea);
+        }
+    }
+
+    public void Imprimir() 
+    {
+        Console.Write("Los 5 sueldos de los operarops \n");
+        for (int f = 1; f < 5; f++)
+        {
+            Console.Write("[" + sueldos[f]+"] ");
+        }
+        Console.ReadKey();  
+    }
+
+    static void Main(string[] args)
+    {
+        Program pv = new Program();
+        pv.Cargar();
+        pv.Imprimir();
+    }
+}
