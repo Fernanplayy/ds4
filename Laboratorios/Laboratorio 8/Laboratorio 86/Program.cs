9645fe01d268fde916efe2ec59afb5f208eda3c7
Fernanplayy <fernangamergamer23@gmail.com>
2025-10-06 14:40:06 -0500

    Laboratorio 8 terminado

--- Laboratorios/Laboratorio 8/Laboratorio 86/Program.cs
+++ Laboratorios/Laboratorio 8/Laboratorio 86/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 
-class ClaseBase {
+class ClaseBase
+{
     public void test()
+    { 
+       
+    }
+    public virtual void masTests() //Sealed da el error de ejecucion, hay que cambiarlo por "virtual"
     { 
     
     }
-    public sealed void MasTests()
+}
+
+class ClaseHijo : ClaseBase
+{
+    public override void masTests()
     { 
-    
+        
+    }
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Corrio la aplicacion");
     }
 }
---
+++ Laboratorios/Laboratorio 8/Laboratorio 87/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+internal class Program { 
+    private static void Main(string[] args) { 
+        Console.WriteLine("Corrio la aplicacion");
+        Console.ReadKey();
+    }
+}
+
+public class ClaseBase //No se puede heredar una clase sealed
+{
+    public void test() 
+    {
+        
+    }
+
+    public void MoreTesting() 
+    {
+    
+    }
+}
+
+class ClaseHijo : ClaseBase
+{
+}
