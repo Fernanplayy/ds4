769c2748f7c36f7e9c0825c9d0da63c2e6df50f8
Fernanplayy <fernangamergamer23@gmail.com>
2025-10-06 18:12:38 -0500

    Avance en laboratorio 9(Faltan los 93->95) y laboratorio 10(Just finish the thingy)

---
+++ Laboratorios/Laboratorio 9/Laboratorio 91/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Laboratorio_91
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double precio;
+            int opcion;
+            string numCuenta;
+
+            Console.WriteLine("Precio del producto: ");
+            precio = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Seleccione el método de pago:");
+            Console.WriteLine("1. Efectivo");
+            Console.WriteLine("2. Tarjeta");
+
+            opcion = int.Parse(Console.ReadLine());
+
+            if (opcion == 1)
+            {
+                Console.WriteLine("Introduzca el dinero en la ranuera de billetes. ----->");
+            }
+            else if (opcion == 2)
+            {
+                Console.WriteLine("Introduzca el numero de cuenta");
+                numCuenta = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Opción no válida");
+            }
+        }
+    }
+}
